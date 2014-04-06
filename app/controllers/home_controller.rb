fedcbf95ecd517eb707ece789be0374d5dd498af
Franco <fnc314@gmail.com>
2014-04-05 19:05:30 -0700

    updating to upstream master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,15 @@ class HomeController < ApplicationController
   end
 
 
-   def sign_in
-    @user = current_user
-     if @user.nil?
-       redirect_to users_path
-     elsif current_user.admin?
-       redirect_to admin_reservations_path
-     else
-       redirect_to new_reservation_path
-     end
-    end
+ def sign_in
+  @user = current_user
+   if @user.nil?
+     redirect_to users_path
+   elsif current_user.admin?
+     redirect_to admin_reservations_path
+   else
+     redirect_to new_reservation_path
+   end
   end
+
+end
